e1a069dc939b2993fe01869931f8be5b4a7da2f5
Diogo Miranda <diogomsmiranda@tecnico.ulisboa.pt>
2024-05-02 10:31:07 +0100

    format controller.js

--- backend/src/api/entrances/controller.js
+++ backend/src/api/entrances/controller.js
@@ -1,7 +1,9 @@
 module.exports = {
   getLemacUserData: async (database, mifare_id) => {
     try {
-      const [results] = await database.execute('SELECT * FROM lemac_users WHERE mifare_id=?', [mifare_id]);
+      const [results] = await database.execute('SELECT * FROM lemac_users WHERE mifare_id=?', [
+        mifare_id,
+      ]);
       return results[0];
     } catch (e) {
       console.error(e);
@@ -19,15 +21,13 @@ module.exports = {
   },
   createUser: async (database, mifare_id, name, ist_id, email, course) => {
     try {
-      await database.execute('INSERT INTO `lemac_users` (mifare_id,  name, ist_id, state, email, course) VALUES ( ? , ? , ? , ? , ? , ?)', [
-        mifare_id,
-        name,
-        ist_id,
-        "offline",
-        email,
-        course
-      ]);
-      const [results] = await database.execute('SELECT * FROM lemac_users WHERE id=LAST_INSERT_ID()');
+      await database.execute(
+        'INSERT INTO `lemac_users` (mifare_id,  name, ist_id, state, email, course) VALUES ( ? , ? , ? , ? , ? , ?)',
+        [mifare_id, name, ist_id, 'offline', email, course]
+      );
+      const [results] = await database.execute(
+        'SELECT * FROM lemac_users WHERE id=LAST_INSERT_ID()'
+      );
       return results[0];
     } catch (e) {
       console.error(e);
@@ -36,15 +36,15 @@ module.exports = {
   },
   changeUserState: async (database, state, user_id) => {
     try {
-      await database.execute('UPDATE lemac_users SET state = ?, last_modified = (now()) WHERE id = ?', [
-        state,
-        user_id,
-      ]);
+      await database.execute(
+        'UPDATE lemac_users SET state = ?, last_modified = (now()) WHERE id = ?',
+        [state, user_id]
+      );
       const [results] = await database.execute('SELECT * FROM lemac_users WHERE id=?', [user_id]);
 
       return results[0];
     } catch (e) {
       console.error(e);
     }
-  }
-}
+  },
+};
