4f1187c177886399b7965c933e8f08bfdf2de0a0
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2025-11-18 01:56:49 +0000

    chore: continued working with typescript

---
+++ frontend/src/api/schedule.api.ts
@@ -0,0 +1,21 @@
+import httpClient from './httpClient.api';
+const ENDPOINT = '/schedule';
+
+export const getHours = (month: number, year: number) =>
+  httpClient.get(ENDPOINT, {
+    params: {
+      month,
+      year,
+    },
+  });
+
+export const createHours = (data: never) => httpClient.post(ENDPOINT, data);
+export const deleteHours = (id: number) => httpClient.delete(`${ENDPOINT}/${id}`);
+export const updateHour = (id: number, data: never) => httpClient.put(`${ENDPOINT}/${id}`, data);
+export const getUserTargets = () => httpClient.get(`${ENDPOINT}/targets`);
+export const setUserTarget = (data: never) => httpClient.post(`${ENDPOINT}/targets`, data);
+export const editUserTarget = (id: number, data: never) => httpClient.put(`${ENDPOINT}/targets/${id}`, data);
+export const deleteUserTarget = (id: number) => httpClient.delete(`${ENDPOINT}/targets/${id}`);
+export const getOffDays = () => httpClient.get(`${ENDPOINT}/off_days`);
+export const setOffDays = (data: never) => httpClient.post(`${ENDPOINT}/off_days`, data);
+export const deleteOffDay = (id: number) => httpClient.delete(`${ENDPOINT}/off_days/${id}`);
