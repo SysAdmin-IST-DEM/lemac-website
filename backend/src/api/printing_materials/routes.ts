445791ef94fc780c9ee86729e789651fe1d010db
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2026-01-12 15:47:52 +0000

    fix: printing materials auth

--- backend/src/api/printing_materials/routes.ts
+++ backend/src/api/printing_materials/routes.ts
@@ -13,7 +13,7 @@ import { AddPrintMaterialBody, EditPrintMaterialBody } from '@lemac/data-model';
 export default {
   init: (app: Express) => {
     app.post('/api/print-materials', requiresAuth(1), parseBody(AddPrintMaterialBody), addPrintingMaterial);
-    app.get('/api/print-materials', requiresAuth(1), getPrintingMaterials);
+    app.get('/api/print-materials', getPrintingMaterials);
     app.put('/api/print-materials/:id', requiresAuth(1), parseBody(EditPrintMaterialBody), updatePrintingMaterial);
     app.delete('/api/print-materials/:id', requiresAuth(1), deletePrintingMaterial);
   },
