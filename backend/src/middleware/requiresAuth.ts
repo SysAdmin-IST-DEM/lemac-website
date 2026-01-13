1dd3fb3584a25993511f24b5635165440b05b664
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2026-01-13 18:16:27 +0000

    fix: issue with tasks not showing to non-admins

--- backend/src/middleware/requiresAuth.ts
+++ backend/src/middleware/requiresAuth.ts
@@ -6,8 +6,6 @@ export function requiresAuth(minRole = 0): RequestHandler {
       return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    console.log("RECEIVED REQ.USER:", req.user, " with admin:", req.user.admin, " and minRole:", minRole);
-
     const role = req.user.admin ? 1 : 0;
 
     return role >= minRole ? next() : res.status(403).json({ error: 'Forbidden' });
