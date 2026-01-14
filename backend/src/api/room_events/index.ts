317c2e6111e81f9cea96413e243d21fcad8d8b83
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2026-01-14 19:54:53 +0000

    fix: reservation events not being added

--- backend/src/api/room_events/index.ts
+++ backend/src/api/room_events/index.ts
@@ -2,18 +2,10 @@ import { RoomEventType } from '@lemac/data-model';
 import * as controller from './controller.js';
 import type { Request, Response } from 'express';
 
-const mapType = {
-  res_created:  RoomEventType.RES_CREATED,
-  res_updated:  RoomEventType.RES_UPDATED,
-  res_deleted:  RoomEventType.RES_DELETED,
-  key_given:    RoomEventType.KEY_GIVEN,
-  key_received: RoomEventType.KEY_RECEIVED,
-};
-
 export async function createEvent(req: Request, res: Response) {
   if (req.body && req.body.type && req.body.roomReservationId) {
     const data = await controller.createEvent(
-      mapType[req.body.type as keyof typeof mapType],
+      req.body.type as RoomEventType,
       req.user!.id,
       req.body.roomReservationId
     );
@@ -41,7 +33,7 @@ export async function editEvent(req: Request, res: Response) {
   const id = parseInt(req.params.id as string);
   if (req.body && req.body.type && req.body.roomReservationId) {
     const data = await controller.editEvent(
-      mapType[req.body.type as keyof typeof mapType],
+      req.body.type as RoomEventType,
       id,
       req.body.roomReservationId,
       req.body.observations
