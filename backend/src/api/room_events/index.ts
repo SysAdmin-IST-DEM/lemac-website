f62938706adc43864cfed656c859898e9c41f00f
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2026-01-14 20:55:34 +0000

    fix: reservation events not being added

--- backend/src/api/room_events/index.ts
+++ backend/src/api/room_events/index.ts
@@ -1,18 +1,10 @@
 import * as controller from './controller.js';
 import type { Request, Response } from 'express';
 
-const eventTypes = {
-  'res_created': 'RES_CREATED',
-  'res_updated': 'RES_UPDATED',
-  'res_deleted': 'RES_DELETED',
-  'key_given': 'KEY_GIVEN',
-  'key_received': 'KEY_RECEIVED'
-}
-
 export async function createEvent(req: Request, res: Response) {
   if (req.body && req.body.type && req.body.roomReservationId) {
     const data = await controller.createEvent(
-      eventTypes[req.body.type], // TODO: temporary fix for prisma issue
+      req.body.type.toUpperCase(), // TODO: temporary fix for prisma issue
       req.user!.id,
       req.body.roomReservationId
     );
@@ -40,7 +32,7 @@ export async function editEvent(req: Request, res: Response) {
   const id = parseInt(req.params.id as string);
   if (req.body && req.body.type && req.body.roomReservationId) {
     const data = await controller.editEvent(
-      eventTypes[req.body.type], // TODO: temporary fix for prisma issue
+      req.body.type.toUpperCase(), // TODO: temporary fix for prisma issue
       id,
       req.body.roomReservationId,
       req.body.observations
