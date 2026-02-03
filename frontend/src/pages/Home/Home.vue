585e05389aa5d51c457088fe25f1ef683fb709bb
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2026-02-03 20:04:57 +0000

    chore: added warning that redirects to student registration

--- frontend/src/pages/Home/Home.vue
+++ frontend/src/pages/Home/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <HomeHeader />
+  <v-alert
+    color="warning"
+    icon="$warning"
+  >
+    Starting <b>March 1, 2026</b>, all students are required to register in LEMAC’s entry system to access
+    the room. Please <RouterLink to="/student-registration">complete your registration</RouterLink>.
+  </v-alert>
   <v-container>
     <RoomMap />
   </v-container>
