a228e6e29cb95cfe09a294f851def938b8519028
Filipe Miranda <filipe.miranda@tecnico.ulisboa.pt>
2026-02-13 20:17:53 +0000

    fix: card reader not working correctly

--- backend/src/api/reader/controller.ts
+++ backend/src/api/reader/controller.ts
@@ -9,7 +9,7 @@ export enum OnScanCardResultCode {
   ACTIVE_ENTRY_FOUND = "ACTIVE_ENTRY_FOUND"
 }
 
-export async function getActiveEntry(mifareNumber: number): Promise<{
+export async function getActiveEntry(mifareNumber: bigint): Promise<{
   ok: boolean;
   code: OnScanCardResultCode;
   student?: Student,
